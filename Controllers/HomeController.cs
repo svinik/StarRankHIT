9a7df2679288a777a4f274a9ac476734aa5b9a7b
Svinik <roysvinik@gmail.com>
2022-03-01 20:22:48 +0200

    changed ssl

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace StarRankHIT.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public async Task<ActionResult> Index()
