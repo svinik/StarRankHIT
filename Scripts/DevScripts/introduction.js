839eae0b9e51784986eedd0202b76013859af5ed
Svinik <roysvinik@gmail.com>
2022-04-17 01:25:52 +0300

    testing submit

--- Scripts/DevScripts/introduction.js
+++ Scripts/DevScripts/introduction.js
@@ -1,3 +1,17 @@
 ﻿function EndInstructions() {
     window.location.replace("/star-rank-exp/Consent/Index");
 }
+
+function submitHIT() {
+    $.ajax({
+        type: "POST",
+        url: "/star-rank-exp/Feedback/SubmitToMturk",
+        data: {},
+        success: function (response) {
+            window.location.replace(response);
+        },
+        error: function (jqXHR, exception) {
+            window.location.replace("/star-rank-exp/Home/Error?lastScreen=feedback");
+        }
+    });
+}
