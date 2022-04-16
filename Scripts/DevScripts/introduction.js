3d8e887110461a10754ee8d701fc2dd6b61dabb4
Svinik <roysvinik@gmail.com>
2022-04-17 02:08:05 +0300

    testing submit again 2

--- Scripts/DevScripts/introduction.js
+++ Scripts/DevScripts/introduction.js
@@ -3,6 +3,6 @@
 }
 
 function submitHIT(assignmentId) {
-    var url = "https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=" + assignmentId;
+    var url = "https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=" + assignmentId + "&foo=bar";
     window.location.replace(url);
 }
